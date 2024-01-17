3479345935f02aa9a013b6d6af258bf81ed7a899
ganeshgani999 <sivaganesh1399705@gmail.com>
2024-01-17 13:40:01 +0000

    Editable Text Input

---
+++ src/components/TextInput/styledComponents.js
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+    background-color: #f5d0fe;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+export const CardContainer = styled.div`
+    width: 400px;
+    height: 200px;
+    background-color: #ffffff;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    border-radius: 7px;
+`
+
+export const Heading = styled.h1`
+    color: #000000;
+    font-size: 32px;
+    font-family: "Roboto";
+`
+export const InputContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+`
+export const Input = styled.input`
+    height: 30px;
+    width: 190px;
+    border: 1px solid #cbd2d9;
+    margin-right: 20px;
+    font-family: "Roboto";
+    color: #323f4b;
+    padding-left: 10px;
+`
+export const Button = styled.button`
+    height: 30px;
+    width: 60px;
+    background-color: #d946ef;
+    border: 0px;
+    border-radius: 5px;
+    font-family: "Roboto";
+    color: #ffffff;
+`
+export const Paragraph = styled.p`
+    color:  #cbd2d9;
+    margin-right: 20px;
+`
